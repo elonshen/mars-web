dd94a0eddaf773b37a9a09e170c924a421dafbfb
elonshen <1216341897@qq.com>
2022-02-20 14:21:55 +0800

    去除vuex browserslist private

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import VueCookies from 'vue-cookies'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
@@ -12,7 +11,6 @@ Vue.use(Element)
 Vue.use(VueCookies)
 new Vue({
   router,
-  store,
   render: function (h) {
     return h(App)
   }
